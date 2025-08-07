46e771ae9428b2f05efa5a13236860d0cd191337
szks-repo <szkscookie@gmail.com>
2025-08-08 00:21:40 +0900

    day1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/szks-repo/gosmarty
+
+go 1.24.5
---
+++ token/token.go
@@ -0,0 +1,72 @@
+package token
+
+// TokenType はトークンの種類を表す文字列です。
+type TokenType string
+
+// Token は字句解析器(Lexer)が生成するトークンを表す構造体です。
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+// Smartyテンプレートで使われるトークンの種類を定義します。
+const (
+	// 特殊なトークン
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Delimiters
+	LBRACE     = "{}"        // {
+	RBRACE     = "}"        // }
+	LDELIM     = "LDELIM"   // Smartyの左デリミタ (e.g., {)
+	RDELIM     = "RDELIM"   // Smartyの右デリミタ (e.g., })
+	LITERAL    = "LITERAL"  // {literal}
+	ENDLITERAL = "/LITERAL" // {/literal}
+	COMMENT    = "COMMENT"  // {* ... *}
+
+	// 識別子 + リテラル
+	IDENT  = "IDENT"  // 変数名など (例: foo, bar)
+	DOLLAR = "$"      // $
+	STRING = "STRING" // "foo" or 'bar'
+	NUMBER = "NUMBER" // 12345
+	TEXT   = "TEXT"   // デリミタの外にあるプレーンなテキスト
+
+	// 演算子
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	EQ    = "=="
+	NOTEQ = "!="
+	LT    = "<"
+	GT    = ">"
+
+	IF         = "if"
+	ELSE       = "else"
+	ELSEIF     = "elseif"
+	ENDIF      = "/if"
+	FOREACH    = "foreach"
+	ENDFOREACH = "/foreach"
+)
+
+var keywords = map[string]TokenType{
+	"if":       IF,
+	"else":     ELSE,
+	"elseif":   ELSEIF,
+	"/if":      ENDIF,
+	"foreach":  FOREACH,
+	"/foreach": ENDFOREACH,
+	"literal":  LITERAL,
+	"/literal": ENDLITERAL,
+}
+
+// LookupIdent は識別子がキーワードかどうかを判定します。
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
