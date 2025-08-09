3009f03da0657285ac0055137a6fdb2cb4ce863e
szks-repo <szkscookie@gmail.com>
2025-08-09 18:10:06 +0900

    go fmt

--- object/null.go
+++ object/null.go
@@ -3,9 +3,9 @@ package object
 type Null struct{}
 
 func (n *Null) Type() ObjectType {
-	return NullType 
+	return NullType
 }
 
-func (n *Null) Inspect() string  { 
-	return "null" 
+func (n *Null) Inspect() string {
+	return "null"
 }
